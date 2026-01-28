5ab79bf9d24ebb3af6f14b796156957f0b2aa17d
劉奶糖 <elietclonesoldier@gmail.com>
2026-01-28 15:40:16 +0800

    88

--- html/test.js
+++ html/test.js
@@ -74,7 +74,7 @@ function renderCards(teamsList) {
             <div class="card-button">
                 <div class="team-city">${t.city || ""}</div>
                 <div class="team-state">${t.state_prov || ""}</div>
-                <div class="team-location">${getHrefFromUrl(tbaUrl) || ""}</div>
+                <div class="team-location">${t.team || ""}</div>
             
 
 
